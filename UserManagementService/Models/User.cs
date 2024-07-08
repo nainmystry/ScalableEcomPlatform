dbc261e807012fb20a1c392d33557807611bbb54
nainmystry <88156958+nainmystry@users.noreply.github.com>
2024-07-08 16:40:16 +0000

    Feat - Adding Namespace

--- UserManagementService/Models/User.cs
+++ UserManagementService/Models/User.cs
@@ -1,6 +1,9 @@
-public class User{
-    public int Id {get; set;}
-    public string Name {get; set;}
-    public string EmailID {get; set;}
-    public string Password {get; set;}        
+namespace UserManagementService.Models
+{
+    public class User{
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string EmailID {get; set;}
+        public string Password {get; set;}        
+    }   
 }
